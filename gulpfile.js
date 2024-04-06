c73d188c84b84239456e1df92236209a910fa691
Wendel <wendel.isaac12@gmail.com>
2024-04-06 18:32:13 -0300

    feat: task de minificação de images

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const {task} = require("gulp")
 const sass = require("gulp-sass")(require("sass"))
 const sourcemap = require("gulp-sourcemaps")
 const uglify = require("gulp-uglify")
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require("gulp-imagemin")
 
 task("sass", ()=>{
     return gulp.src("src/styles/*.scss")
@@ -12,7 +14,23 @@ task("sass", ()=>{
     .pipe(gulp.dest("build/styles"))
 })
 
-
-task("watch-sass",()=>{
-    return gulp.watch("src/styles/*.scss", gulp.series("sass"))
+task("miniJS", ()=>{
+    return gulp.src("src/js/*.js")
+    .pipe(uglify())
+    .pipe(obfuscate())
+    .pipe(gulp.dest("build/js"))
 })
+
+task("miniIMG", ()=>{
+    return gulp.src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("build/images"))
+})
+
+task("watch",()=>{
+    gulp.watch("src/styles/*.scss", gulp.series("sass"))
+    gulp.watch("src/js/*.js", gulp.series("miniJS"))
+    gulp.watch("src/images/*", gulp.series("miniIMG"))
+})
+
+task("default", gulp.series("watch"))
