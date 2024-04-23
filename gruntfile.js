f701dbff9ace0d60640dcd065f72688559322036
Wendel <wendel.isaac12@gmail.com>
2024-04-23 02:21:55 -0300

    feat: criação de tasks para build do projeto

--- gruntfile.js
+++ gruntfile.js
@@ -2,24 +2,77 @@ module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         sass:{
-            dist:{
-                files: { "dist/styles/style.css" : "src/styles/style.scss"}
+            dev:{
+                files:{
+                    "dev/styles/style.css" : "src/styles/style.scss"
+                }
             },
-            options:{
-                style: "compressed"
+            prod:{
+                options:{
+                    style: "compressed"
+                },
+                files: { "dist/styles/style.min.css" : "src/styles/style.scss"}
             }
         },
+
+        uglify:{
+            target:{
+                files:{"dist/scripts/script.min.js" : "src/scripts/script.js"}
+            }
+        },
+
+        htmlmin:{
+            prod:{
+                options:{
+                    removeComments: true,
+                    collapseWhitespace:true
+                },
+                files: {
+                    "prebuild/index.html" : "src/index.html"
+                }
+            }
+        },
+
+        replace:{
+            dev:{
+                options: {
+                    patterns: [
+                        { match: "css", replacement: "styles/style.css" }, 
+                        { match: "js", replacement: "../src/scripts/script.js" }
+                    ]
+                },
+                files: [{ expand: true, flatten: true, src: ["src/index.html"], dest: "dev/"}]
+            },
+            prod:{
+                options: {
+                    patterns: [
+                        { match: "css", replacement: "styles/style.min.css" }, 
+                        { match: "js", replacement: "scripts/script.min.js" }
+                    ]
+                },
+                files: [{ expand: true, flatten: true, src: ["prebuild/index.html"], dest: "dist/"}]
+            },
+            
+        },
+
         watch:{
             sass:{
                 files: ["src/styles/*.scss"],
-                tasks: ['sass']
+                tasks: ['sass:dev']
             }
             
-        }
+        },
+        clean:["prebuild"]
     })
 
     grunt.loadNpmTasks("grunt-contrib-sass")
+    grunt.loadNpmTasks("grunt")
+    grunt.loadNpmTasks("grunt-replace")
+    grunt.loadNpmTasks("grunt-contrib-htmlmin")
+    grunt.loadNpmTasks("grunt-contrib-uglify")
     grunt.loadNpmTasks("grunt-contrib-watch")
+    grunt.loadNpmTasks("grunt-contrib-clean")
 
-    grunt.registerTask("default", "watch")
+    grunt.registerTask("default", ["sass:dev", "replace:dev"])
+    grunt.registerTask("build", ["sass:prod", "uglify", "htmlmin:prod","replace:prod", "clean"])
 }
