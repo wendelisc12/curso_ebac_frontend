3793548d939a237e1d1e9b0a09b7ed3eb372ebab
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2024-06-26 02:55:05 -0300

    Create script.ts

---
+++ script.ts
@@ -0,0 +1,12 @@
+
+function multiplicacao(primeiro:number, segundo:number):number{
+    return primeiro * segundo
+  }
+  
+  function saudação(nome:string):string{
+    return `olá ${nome}`
+  }
+  
+  console.log(multiplicacao(20, 42))
+  console.log(saudação("wendel"))
+  
